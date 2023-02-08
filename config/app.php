380f1404c6747c39762b4f488f4ac636271a8453
JHacin <jan.hacin@gmail.com>
2023-02-08 18:28:10 +0100

    [deployment] remove laravel ide helper from app providers

--- config/app.php
+++ config/app.php
@@ -187,7 +187,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...

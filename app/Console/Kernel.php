b91907a4dd6a78a78a0ac7ac8d3e97947f1141e4
JHacin <jan.hacin@gmail.com>
2024-02-29 15:07:30 +0100

    remove GH action / deployer / scheduled backups

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,8 +9,8 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('backup:clean')->daily()->at('04:00');
-        $schedule->command('backup:run')->daily()->at('05:00');
+        // $schedule->command('backup:clean')->daily()->at('04:00');
+        // $schedule->command('backup:run')->daily()->at('05:00');
     }
 
     protected function commands(): void
